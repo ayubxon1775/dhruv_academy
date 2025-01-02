18d91b7bbb244a8789c486057946938594b41366
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-01-02 10:37:45 +0500

    work section

--- my-project/src/components/work/Work.jsx
+++ my-project/src/components/work/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Dhruv1 from '../../assets/dhruv1.png';
 import Dhruv2 from '../../assets/dhruv2.png';
 import Dhruv3 from '../../assets/dhruv3.jpeg'
+import {GoVerified} from 'react-icons/go'
 
 const CardData = [
   {
@@ -15,7 +16,7 @@ const CardData = [
   },
   {
     id: 2,
-    channelName: 'Dhruv Rather Vlogs',
+    channelName: 'Dhruv Rathee Vlogs',
     handle: 'DhruvRatheeVlogs',
     subscribers: '12M+',
     videos: '540+',
@@ -24,7 +25,7 @@ const CardData = [
   },
   {
     id: 3,
-    channelName: 'Dhruv Rather',
+    channelName: 'Dhruv Rathee short',
     handle: 'dhruvrathee',
     subscribers: '12M+',
     videos: '540+',
@@ -40,22 +41,30 @@ const Work = () => {
     <>
     <section>
       <div className='container py-12 md:py-24'>
-      <h1 className='uppercase text-3xl md:text-5xl font-bold text-center'>My work</h1>
+      <h1 className='mb-14 uppercase text-3xl md:text-5xl font-bold text-center'>My work</h1>
 
       {/* card section */}
     <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
       {CardData.map((data) => {
         return (
           <div key={data.id} className='p-4 rounded-3xl md:p-8 bg-[#1b1b1b]'>
-            <img src={data.image} alt="" className='w-full rounded-2xl' />
-            <div className='text-center pt-5 space-y-3'>
-              <p className='text-4xl md:text-5xl font-bold'>{data.channelName}</p>
+            <img src={data.image} alt="" className='w-full rounded-2xl'/>
+            <div className='flex flex-col justify-between h-[300px]'>
+            <div className='text-center pt-7 space-y-4'>
+              <p className='text-4xl md:text-5xl font-bold'>{data.channelName} {''} 
+              <span className='inline-block pl-4'> <GoVerified className='text-[24px]'/> </span>
+               </p>
               <p>@{data.handle}</p>
-              <div>
-              <p>{data.subscribers} subscribers</p>
+              <div className='flex justify-center gap-4'>
+              <p>{data.subscribers} Subscribers</p>
               <p>{data.videos} videos</p>
               </div>
             </div>
+            <div>
+            <button className=' inline-block uppercace border w-full py-4 border-white text-white rounded-full px-8'>View Channel</button>
+            </div>
+            </div>
+            
           </div>
         )
       })}
